e42a49c667b065848743ed268442b830d2c84579
ShinyMiraidon <bind-naps0r@icloud.com>
2023-02-16 19:36:47 -0500

    added ALU module and test

--- dv/alu.cpp
+++ dv/alu.cpp
@@ -1,7 +1,117 @@
 #include <catch2/catch_test_macros.hpp>
 #include <VAlu.h>
+#include <cstdint>
+#include <stdlib.h>  
+#include <math.h>
 
+uint32_t test(uint32_t a, uint32_t b, uint8_t op1, uint8_t op2) {
+    VAlu model;
+    model.op_code_1 = op1;
+    model.op_code_2 = op2;
+    model.operand_a = a;
+    model.operand_b = b;
+    model.eval();
+    return model.out;
+}
 
-TEST_CASE("ALU") {
-  VAlu model;
+TEST_CASE("ADD") {
+    for (int i = 0; i < 1000; i++) {
+        uint32_t a = rand() % (int) (pow(2, 32) - 1);
+        uint32_t b = rand() % (int) (pow(2, 32)  - 1);
+        uint32_t result = test(a, b, 0, 0);
+        REQUIRE(result == (uint32_t) (a + b));
+    }
+}
+
+TEST_CASE("SUB") {
+    for (int i = 0; i < 1000; i++) {
+        uint32_t a = rand() % (int) (pow(2, 32) - 1);
+        uint32_t b = rand() % (int) (pow(2, 32)  - 1);
+        uint32_t result = test(a, b, 0, 1);
+        REQUIRE(result == (uint32_t) (a + (~b + 1)));
+    }
+}
+
+TEST_CASE("XOR") {
+    for (int i = 0; i < 1000; i++) {
+        uint32_t a = rand() % (int) (pow(2, 32) - 1);
+        uint32_t b = rand() % (int) (pow(2, 32)  - 1);
+        uint32_t result = test(a, b, 4, 0);
+        REQUIRE(result == (a ^ b));
+    }
+}
+
+TEST_CASE("OR") {
+    for (int i = 0; i < 1000; i++) {
+        uint32_t a = rand() % (int) (pow(2, 32) - 1);
+        uint32_t b = rand() % (int) (pow(2, 32)  - 1);
+        uint32_t result = test(a, b, 6, 0);
+        REQUIRE(result == (a | b));
+    }
+}
+
+TEST_CASE("AND") {
+    for (int i = 0; i < 1000; i++) {
+       uint32_t a = rand() % (int) (pow(2, 32) - 1);
+        uint32_t b = rand() % (int) (pow(2, 32)  - 1);
+        uint32_t result = test(a, b, 7, 0);
+        REQUIRE(result == (a & b));
+    }
+}
+
+TEST_CASE("LLS") {
+    for (int i = 0; i < 1000; i++) {
+        uint32_t a = rand() % (int) (pow(2, 32) - 1);
+        uint32_t b = rand() % (int) (pow(2, 32)  - 1);
+        uint32_t result = test(a, b, 1, 0);
+        REQUIRE(result == (a << (b & 31)));
+    }
+}
+
+TEST_CASE("LRS") {
+    for (int i = 0; i < 1000; i++) {
+        uint32_t a = rand() % (int) (pow(2, 32) - 1);
+        uint32_t b = rand() % (int) (pow(2, 32)  - 1);
+        uint32_t result = test(a, b, 5, 0);
+        REQUIRE(result == (a >> (b & 31)));
+    }
+}
+
+TEST_CASE("ARS") {
+    for (int i = 0; i < 1000; i++) {
+        uint32_t a = rand() % (int) (pow(2, 32) - 1);
+        uint32_t b = rand() % (int) (pow(2, 32)  - 1);
+        uint32_t result = test(a, b, 5, 1);
+        uint32_t expected;
+        if (a >> 31) {
+            expected = (a >> (b & 31));
+            for (int i = 32 - (b & 31); i < 32; i++) {
+                expected += 1 << i;
+            }
+        }
+        else {
+        expected = (a >> (b & 31));
+        }
+        
+        REQUIRE(result == expected);
+    }
+  
+}
+
+TEST_CASE("SSLT") {
+    for (int i = 0; i < 1000; i++) {
+        uint32_t a = rand() % (int) (pow(2, 32) - 1);
+        uint32_t b = rand() % (int) (pow(2, 32)  - 1);
+        uint32_t result = test(a, b, 2, 0);
+        REQUIRE(result == (((a & (1 << 31)) & ~(b & (1 << 31)))? 1 : (~(a & (1 << 31)) & (b & (1 << 31))) ? 0 : a < b));
+    }
+}
+
+TEST_CASE("USLT") {
+    for (int i = 0; i < 1000; i++) {
+        uint32_t a = rand() % (int) (pow(2, 32) - 1);
+        uint32_t b = rand() % (int) (pow(2, 32)  - 1);
+        uint32_t result = test(a, b, 3, 0);
+        REQUIRE(result == (a < b));
+    }
 }
--- rtl/Alu.sv
+++ rtl/Alu.sv
@@ -1,3 +1,50 @@
-module Alu ();
+
+//ALU opcode definitions
+parameter ADD = 5'h00;
+parameter SUB = 5'h10;
+parameter XOR = 5'h04;
+parameter OR = 5'h06;
+parameter AND = 5'h07;
+parameter LLS = 5'h01;
+parameter LRS = 5'h05;
+parameter ARS = 5'h15;
+parameter SSLT = 5'h02;
+parameter USLT = 5'h03;
+
+
+module Alu (
+    input [31:0] operand_a, operand_b,
+    input [3:0] op_code_1,
+    input op_code_2,
+    output logic [31:0] out,
+    logic[4:0] op_code,
+    logic[31:0] adder_result,
+    logic do_sub, carry
+);
+
+assign op_code = {op_code_2, op_code_1};
+
+assign do_sub = op_code_2 | (op_code == SSLT) | (op_code == USLT);
+
+assign {carry, adder_result} = {1'b0, operand_a} + {1'b0, ((operand_b^({32{do_sub}})) + {31'b0, do_sub})};
+
+always_comb begin 
+    case(op_code)
+    ADD: out = adder_result;
+    SUB: out = adder_result;
+    XOR: out = operand_a ^ operand_b;
+    OR: out = operand_a | operand_b;
+    AND: out = operand_a & operand_b;
+    LLS: out = operand_a << operand_b[4:0];
+    LRS: out = operand_a >> operand_b[4:0];
+    ARS: out = operand_a >>> operand_b[4:0];
+    SSLT: out = (operand_a[31] & !(operand_b[31])) ? 1 : (!(operand_a[31]) & operand_b[31]) ? 0 : {31'b0, carry^(!operand_b[31])};
+    USLT: out = {31'b0, !carry};
+    default: out = 0;
+    endcase
+
+
+end
+
 
 endmodule
