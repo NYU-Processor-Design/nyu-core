3648b55f0a284aa949b7d28ac37fc5e64be16410
ShinyMiraidon <bind-naps0r@icloud.com>
2023-03-27 14:33:31 -0400

    Fixed bugs to ensure tests pass

--- dv/branch_eval.cpp
+++ dv/branch_eval.cpp
@@ -1,5 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
-#include <VBranch_EVal.h>
+#include <VBranch_Eval.h>
 #include <cstdint>
 #include <stdlib.h>  
 #include <math.h>
@@ -9,7 +9,7 @@ bool test(uint32_t alu_out, uint8_t cond) {
     model.ALU_Out = alu_out;
     model.Cond = cond;
     model.eval();
-    return model.out;
+    return model.Branch_Taken;
 }
 
 TEST_CASE("Never Branch") {  //Case when non branching instruction
@@ -26,7 +26,7 @@ TEST_CASE("Branch if ALU_OUT is Non-Zero") { //Case when branch condition is < o
         uint32_t alu_out = rand() % (int) (pow(2, 32) - 1);
         uint8_t cond = 1;
         bool result = test(alu_out, cond);
-        REQUIRE(result == |alu_out);
+        REQUIRE(result == (bool) alu_out);
     }
 }
 
@@ -34,9 +34,9 @@ TEST_CASE("Branch if ALU_OUT is Non-Zero") { //Case when branch condition is < o
 TEST_CASE("Branch if ALU_OUT is Zero") { //Case when branch condition is >= or =
     for (int i = 0; i < 1000; i++) {
         uint32_t alu_out = rand() % (int) (pow(2, 32) - 1);
-        uint8_t cond = 1;
+        uint8_t cond = 2;
         bool result = test(alu_out, cond);
-        REQUIRE(result == ~(|alu_out));
+        REQUIRE(result != (bool) alu_out);
     }
 }
 
--- rtl/Branch_Eval.sv
+++ rtl/Branch_Eval.sv
@@ -2,8 +2,8 @@
 //Branch condition definitions
 parameter NE = 2'h0; //Non branching instruction
 parameter ALU = 2'h1; //Branch condition is < or !=
-parameter NALU = 2'h02; //Branch condition is >= or =
-parameter AL = 2'h03; //Jump instruction
+parameter NALU = 2'h2; //Branch condition is >= or =
+parameter AL = 2'h3; //Jump instruction
 
 
 
@@ -11,17 +11,17 @@ module Branch_Eval #(
   WordSize = 32
 )(
     input [WordSize - 1:0] ALU_Out,
-    input [3:0] Cond,
+    input [1:0] Cond,
     output logic Branch_Taken
 );
 
 always_comb begin 
     case(Cond)
-    NE: Branch_Taken = 0;  
-    ALU: Branch_Taken = |ALU_Out
-    NALU: Branch_Taken = ~(|ALU_Out)
-    AL: Branch_Taken = 1;
-    default: Branch_Taken = 0;
+        NE: Branch_Taken = 0;  
+        ALU: Branch_Taken = |ALU_Out;
+        NALU: Branch_Taken = ~(|ALU_Out);
+        AL: Branch_Taken = 1;
+        default: Branch_Taken = 0;
     endcase
 end
 
