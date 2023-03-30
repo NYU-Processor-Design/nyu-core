553ac880b2f82fc0f98af11cb1b670b95d143d57
umanachi <123425184+umanachi@users.noreply.github.com>
2023-03-29 23:02:30 -0400

    reupload PC module

---
+++ rtl/PC.sv
@@ -0,0 +1,18 @@
+
+module PC #(
+  WordSize = 32
+)(
+  input clk,
+  input rstn,
+  input [WordSize - 1:0] npc,
+  output logic [WordSize - 1:0] pc
+);
+  
+  always @ (posedge clk or negedge rstn) begin
+    if (rstn == 0)
+      pc <= 0;
+    else
+      pc <= npc;
+  end
+  
+endmodule
