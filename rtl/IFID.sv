75000cc2fa7f2607b79b1157720340d3272057ad
ShinyMiraidon <bind-naps0r@icloud.com>
2023-04-13 14:28:28 -0400

    Finished IFID Module and Tests

--- rtl/IFID.sv
+++ rtl/IFID.sv
@@ -11,26 +11,27 @@ module IFID #(
 );
   
   always @ (posedge clk or negedge rstn) begin
-    if (!rstn)
+    if (!rstn) begin
       rdn <= 0;
       rs1n <= 0;
       rs2n <= 0;
       pc <= 0;
-      imm <= 0;
-    else
+      imm = 0;
+    end
+    else begin
       pc <= pc_in;
       rdn <= ins[11:7];
       rs1n <= ins[19:15];
       rs2n <= ins[24:20];
       case(immode)
         1: imm = {{20{ins[31]}}, ins[31:20]};
-        2: imm = {{20{ins[31]}}, ins[31:25], ins[11:7]}
-        3: imm = {{19{ins[31]}}, ins[31], ins[7], ins[30:25], ins[11:8], 0}
-        4: imm = {ins[31:12], 12'b0}
-        5: imm = {12'b0, ins[31], ins[19:12], ins[20], ins[30:21], 0}
+        2: imm = {{20{ins[31]}}, ins[31:25], ins[11:7]};
+        3: imm = {{19{ins[31]}}, ins[31], ins[7], ins[30:25], ins[11:8], 1'b0};
+        4: imm = {ins[31:12], 12'b0};
+        5: imm = {11'b0, ins[31], ins[19:12], ins[20], ins[30:21], 1'b0};
       default imm = 0;
       endcase
-
+    end
       
   end
   
