f8babe53b4aed5e5c0b12889e39eac89df654034
ShinyMiraidon <bind-naps0r@icloud.com>
2023-09-07 16:24:30 -0400

    Added start of MEM/WB latch test

---
+++ dv/memwb.cpp
@@ -0,0 +1,14 @@
+#include <catch2/catch_test_macros.hpp>
+#include <VMEMWB.h>
+#include <cstdint>
+#include <stdlib.h>
+#include <math.h>
+
+VMEMWB model;
+bool clk;
+bool rstn;
+
+
+TEST_CASE() {
+ 
+}
