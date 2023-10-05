7b1e80731d925ab1e3411ac930c4017cdeff0a04
umanachi <123425184+umanachi@users.noreply.github.com>
2023-10-05 14:14:51 -0400

    fix(tests): merge branch tests

--- dv/exmem.cpp
+++ dv/exmem.cpp
@@ -36,7 +36,7 @@ TEST_CASE("Reset"){
   }
 }
 
-TEST_CASE("Branch Taken 0") {
+TEST_CASE("ExMem Latch") {
   VEXMEM model;
   bool clk;
   bool rstn;
@@ -49,6 +49,7 @@ TEST_CASE("Branch Taken 0") {
     rdn_in = rand() % (int) (pow(2,5) - 1);
     alu_out_in = rand() % (int) (pow(2,32) - 1);
     rs2d = rand() % (int) (pow(2,32) - 1);
+    branch_taken_in = rand() % 2;
 
     // reset module
     model.rstn = 1;
@@ -63,47 +64,11 @@ TEST_CASE("Branch Taken 0") {
     model.rdn_in = rdn_in;
     model.alu_out_in = alu_out_in;
     model.rs2d = rs2d;
-    model.branch_taken_in = 0;
+    model.branch_taken_in = branch_taken_in;
     model.eval();
     REQUIRE(model.rdn == rdn_in);
     REQUIRE(model.alu_out == alu_out_in);
-    REQUIRE(model.branch_taken == 0);
-    REQUIRE(model.mem_data == rs2d);
-  }
-}
-
-TEST_CASE("Branch Taken 1") {
-  VEXMEM model;
-  bool clk;
-  bool rstn;
-  uint8_t rdn_in;
-  uint32_t alu_out_in;
-  uint32_t rs2d;
-  bool branch_taken_in;
-
-  for (int i = 0; i < 1000; i++) {
-    rdn_in = rand() % (int) (pow(2,5) - 1);
-    alu_out_in = rand() % (int) (pow(2,32) - 1);
-    rs2d = rand() % (int) (pow(2,32) - 1);
-
-    // reset module
-    model.rstn = 1;
-    model.clk = 0;
-    model.eval();
-    model.rstn = 0;
-    model.eval();
-
-    // set all registers
-    model.rstn = 1;
-    model.clk = 1;
-    model.rdn_in = rdn_in;
-    model.alu_out_in = alu_out_in;
-    model.rs2d = rs2d;
-    model.branch_taken_in = 1;
-    model.eval();
-    REQUIRE(model.rdn == rdn_in);
-    REQUIRE(model.alu_out == alu_out_in);
-    REQUIRE(model.branch_taken == 1);
+    REQUIRE(model.branch_taken == branch_taken_in);
     REQUIRE(model.mem_data == rs2d);
   }
 }
