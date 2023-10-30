91be0992d1e53e0bc71c92a9ad0ab9bbac885465
Rielle Lim <2gabriellelim@gmail.com>
2023-10-29 21:10:19 -0400

    Update Branch_Addr_Calc.sv

--- rtl/Branch_Addr_Calc.sv
+++ rtl/Branch_Addr_Calc.sv
@@ -1,26 +1,24 @@
 //Branch Address Conditions
 parameter PC = 0;   //Case when branch_addr = pc + imm
 parameter RD = 1;   //Case when branch_addr = imm + rs1d
-parameter BNT = 0;  //Case when branch is taken
-parameter BT = 1;  //Case when branch is NOT taken
 
 module Branch_Addr_Calc # (
     WordSize = 32
 )(
     input addr_mode, branch_taken,
-    input [WordSize - 1:0] imm, rs1d,
+    input [WordSize - 1:0] imm, rs1d, pc_in,
     output logic [WordSize - 1:0] branch_addr, npc
 );
 
 always_comb begin
     case(addr_mode)
         PC: begin
-            branch_addr = pc + imm;
+            branch_addr = pc_in + imm;
             if (branch_taken) begin
                 npc = branch_addr;
             end
             else begin
-                npc = pc;
+                npc = pc_in;
             end
         end
         RD: begin
@@ -29,12 +27,12 @@ always_comb begin
                 npc = branch_addr;
             end
             else begin
-                npc = pc;
+                npc = pc_in;
             end
         end
         default: begin
-            branch_addr = pc + imm;
-            npc = pc;
+            branch_addr = pc_in + imm;
+            npc = pc_in;
         end
     endcase
 end
