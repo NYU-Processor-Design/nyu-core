db09c16bca30d053fd52123d9a7cc06e5b76a34b
ShinyMiraidon <114675487+ShinyMiraidon@users.noreply.github.com>
2023-10-31 11:10:00 -0400

    Update EXMEM.sv removed branch taken'

--- rtl/EXMEM.sv
+++ rtl/EXMEM.sv
@@ -4,8 +4,6 @@ module EXMEM #(
   input clk, rstn,
   input [4:0] rdn_in,
   input [WordSize - 1:0] alu_out_in, rs2d,
-  input branch_taken_in,
-  output logic branch_taken,
   output logic [4:0] rdn,
   output logic [WordSize - 1:0] alu_out, mem_data
 );
@@ -15,12 +13,10 @@ module EXMEM #(
       rdn <= 0;
       alu_out <= 0;
       mem_data <= 0;
-      branch_taken <= 0;
     end
     else begin
       rdn <= rdn_in;
       alu_out <= alu_out_in;
-      branch_taken <= branch_taken_in;
       mem_data <= rs2d;
     end
   end
