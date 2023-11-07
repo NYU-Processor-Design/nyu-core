678a4877f8cf49184b8993acc427b1d9acda54a5
ShinyMiraidon <114675487+ShinyMiraidon@users.noreply.github.com>
2023-11-07 14:11:15 -0500

    add(Branch_Predictor.sv, branch_predictor.cpp) (#111)

---
+++ dv/branch_predictor.cpp
@@ -0,0 +1,102 @@
+#include <catch2/catch_test_macros.hpp>
+#include <VBranch_Predictor.h>
+#include <cstdint>
+#include <stdlib.h>  
+#include <math.h>
+
+
+TEST_CASE("Non Prediction Instruction") {
+
+    VBranch_Predictor model;
+
+    uint8_t branch_occr;
+    bool act_taken;
+    bool pred_taken;
+    uint8_t branch_cond;
+
+    //Initialize Module
+    model.rstn_h = 1;
+    model.clk = 0;
+    model.eval();
+    model.rstn_h = 0;
+    model.eval();
+
+    
+
+    //Test Module
+    for (int i = 0; i < 1000; i++) {
+        branch_occr = rand() % (int) (pow(2, 1) - 1);
+        act_taken = rand() % (int) (pow(2, 1) - 1);
+        pred_taken = rand() % (int) (pow(2, 1) - 1);
+        branch_cond = rand() % (int) (pow(2, 2) - 1);
+
+        model.clk = 0;
+        model.eval();
+
+        model.clk = 1;
+        model.rstn_h = 1;
+        model.branch_occr = branch_occr;
+        model.act_taken = act_taken;
+        model.pred_taken = pred_taken;
+        model.branch_cond = branch_cond;
+        model.eval();
+        REQUIRE ((bool) model.branch_taken == (bool) branch_occr);
+    }
+}
+
+TEST_CASE("Prediction Instruction") {
+
+    VBranch_Predictor model;
+
+    uint8_t branch_occr;
+    bool act_taken;
+    bool pred_taken;
+    uint8_t branch_cond;
+
+    //Initialize Module
+    model.rstn_h = 1;
+    model.clk = 0;
+    model.eval();
+    model.rstn_h = 0;
+    model.eval();
+
+    //Initialize Register Tracker Variables
+    bool curr_pred = 0;
+    bool incorrect_pred = 0;
+
+    //Test Module
+    for (int i = 0; i < 1000; i++) {
+        branch_occr = 2 + rand() % (int) (pow(2, 1) - 1);
+        act_taken = rand() % (int) (pow(2, 1) - 1);
+        pred_taken = rand() % (int) (pow(2, 1) - 1);
+        branch_cond = rand() % (int) (pow(2, 2) - 1);
+        
+        model.clk = 0;
+        model.eval();
+
+        model.clk = 1;
+        model.rstn_h = 1;
+        model.branch_occr = branch_occr;
+        model.act_taken = act_taken;
+        model.pred_taken = pred_taken;
+        model.branch_cond = branch_cond;
+
+        if (__builtin_parity(branch_cond) == 0) {
+            break;
+        }
+        else if ((act_taken ^ pred_taken) == 0) {
+            incorrect_pred = 0;
+        }
+        else if (incorrect_pred == 1) {
+            curr_pred = ~curr_pred;
+            incorrect_pred = 1;
+        }
+        else {
+            incorrect_pred = 1;
+        }
+
+        model.eval();
+
+        REQUIRE ((bool) model.branch_taken == (bool) curr_pred);
+    }
+}
