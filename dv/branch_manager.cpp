8407eed06ff364ed7b70b9a11a3afc5376599f82
ShinyMiraidon <bind-naps0r@icloud.com>
2023-11-07 14:31:28 -0500

    add(branch_manager.cpp, Branch_Manager.sv)

---
+++ dv/branch_manager.cpp
@@ -0,0 +1,23 @@
+#include <catch2/catch_test_macros.hpp>
+#include <VBranch_Manager.h>
+#include <cstdint>
+#include <stdlib.h>  
+#include <math.h>
+
+TEST_CASE("") {
+
+    VBranch_Manager model;
+   
+    //Initialize Module
+    model.rstn = 1;
+    model.clk = 0;
+    model.eval();
+    model.rstn_h = 0;
+    model.eval();
+
+    
+    //Test Module
+    for (int i = 0; i < 1000; i++) {
+        
+    }
+}
---
+++ rtl/Branch_Manager.sv
@@ -0,0 +1,34 @@
+module Branch_Manager (
+    input clk, rstn, pred_taken, act_taken
+    input [WordSize - 1:0] pred_pc, pred_addr
+    output logic flush
+    output logic [WordSize - 1:0] npc
+);
+
+logic restart;
+
+always @ (posedge clk or negedge rstn_h) begin
+    if (!rstn) begin
+      restart <= 1;
+      flush <= 0;
+      npc <= 0;
+    end
+    else begin
+      if ((pred_taken != act_taken) && !restart) begin
+        flush <= 1;
+        case(act_taken)
+        0: npc <= pred_pc + 4
+        1: npc <= pred_addr
+        endcase
+      end
+      else begin
+        flush <= 0;
+        case(pred_taken)
+        0: npc <= pred_pc + 4
+        1: npc <= pred_addr
+        endcase
+      end
+      if (restart) restart <= 0;
+    end
+end
+endmodule
