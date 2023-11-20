0c193857d58be0de180dde15a66ca0e57b959559
ShinyMiraidon <bind-naps0r@icloud.com>
2023-11-20 11:07:58 -0500

    interface(Data_Cache_Manager_Control): Fixed names

--- rtl/Data_Cache_Manager_Control.sv
+++ rtl/Data_Cache_Manager_Control.sv
@@ -3,12 +3,12 @@ interface Data_Cache_Manager_Control();
 logic dcache_en, dcache_rw;
 logic [1:0] data_mode;
 
-modport if_id (
+modport general_control (
 output dcache_en, dcache_rw,
 output [1:0] data_mode
 );
 
-modport id_ex {
+modport data_cache_manager {
 input dcache_en, dcache_rw,
 input [1:0] data_mode
 };
