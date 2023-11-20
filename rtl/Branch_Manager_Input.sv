cfeeb2b0a89ded207e5aa62f309c9c6262b9269d
ShinyMiraidon <bind-naps0r@icloud.com>
2023-11-20 11:11:32 -0500

    add(Branch_Manager_Input.sv): Added Interface

---
+++ rtl/Branch_Manager_Input.sv
@@ -0,0 +1,17 @@
+interface Branch_Manager_Input #(
+  WordSize = 32
+)();
+
+logic [WordSize - 1:0] pc, addr;
+logic taken;
+
+modport id_ex (
+output [WordSize - 1:0] pc, addr,
+output taken
+);
+
+modport branch_manager {
+input [WordSize - 1:0] pc, addr,
+input taken
+};
+endinterface
