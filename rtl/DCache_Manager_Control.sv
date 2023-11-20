8b3ab320e7f9bfab0092a90fd64c49762c1bdbb4
ShinyMiraidon <bind-naps0r@icloud.com>
2023-11-20 11:16:35 -0500

    interface(DCache_Manager_Control.sv): Renamed File

---
+++ rtl/DCache_Manager_Control.sv
@@ -0,0 +1,15 @@
+interface DCache_Manager_Control();
+
+logic dcache_en, dcache_rw;
+logic [1:0] data_mode;
+
+modport general_control (
+output dcache_en, dcache_rw,
+output [1:0] data_mode
+);
+
+modport dcache_manager {
+input dcache_en, dcache_rw,
+input [1:0] data_mode
+};
+endinterface
