2cb12453a57a474266e8fd2b501410e25431ff70
ShinyMiraidon <114675487+ShinyMiraidon@users.noreply.github.com>
2023-11-20 14:28:44 -0500

    Update Int_DReg_Info.sv

--- rtl/Int_DReg_Info.sv
+++ rtl/Int_DReg_Info.sv
@@ -6,12 +6,10 @@ logic [WordSize - 1:0] rdd;
 logic [4:0] rdn;
 
 modport mem_wb (
-output [WordSize - 1:0] rdd,
-output [4:0] rdn
+output rdd, rdn
 );
 
 modport registers (
-input [WordSize - 1:0] rdd,
-input [4:0] rdn
+  input rdd, rdn
 );
 endinterface
