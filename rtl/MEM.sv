5819a661c377aa90fd621d140d62b447d6572e2f
ShinyMiraidon <bind-naps0r@icloud.com>
2023-11-20 10:57:09 -0500

    add(MEM.sv): Added MEM interface

---
+++ rtl/MEM.sv
@@ -0,0 +1,17 @@
+interface MEM #(
+  WordSize = 32
+)();
+
+logic [WordSize - 1:0] alu_out,
+logic [4:0] rdn;
+
+modport ex_mem (
+output [WordSize - 1:0] alu_out,
+output [4:0] rdn
+);
+
+modport mem/wb {
+input [WordSize - 1:0] alu_out,
+input [4:0] rdn
+};
+endinterface
