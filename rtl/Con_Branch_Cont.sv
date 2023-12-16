891df50b01528f2a116bb902012f886e903dba7a
Xingzhi Dai <xd2305@nyu.edu>
2023-12-15 23:53:28 -0500

    update

--- rtl/Con_Branch_Cont.sv
+++ rtl/Con_Branch_Cont.sv
@@ -12,7 +12,7 @@ logic act_taken,flush;
 logic [WordSize-1:0] npc_corr;
 
 Pipeline_Reset #(WordSize) pipeline_reset_module(.npc_in(npc_in),.npc_corr(npc_corr),.flush(flush),.npc(npc),.rstn_out(rstn_out));
-Branch_Predictor #(WordSize) branch_predictor(.clk(clk),.rstn_h(rstn_h),.act_taken(act_taken),.pred_taken(pred_taken),.branch_occr(branch_occr),branch_cond(branch_cond),.branch_taken(branch_taken));
+Branch_Predictor #(WordSize) branch_predictor(.clk(clk),.rstn_h(rstn_h),.act_taken(act_taken),.pred_taken(pred_taken),.branch_occr(branch_occr),.branch_cond(branch_cond),.branch_taken(branch_taken));
 Branch_Manager #(WordSize) branch_manager(.clk(clk),.rstn(rstn),.pred_taken(pred_taken),.pred_pc(pred_pc),.npc(npc));
 Branch_Eval #(WordSize) branch_eval(.alu_out(alu_out),.branch_cond(branch_cond),.act_taken(act_taken));
 
