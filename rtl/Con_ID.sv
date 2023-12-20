9454881b5f2dcf5e26848476da5de7f90b8c7ec9
ShinyMiraidon <bind-naps0r@icloud.com>
2023-12-20 12:11:31 -0500

    connection_module(Con_ID.sv): Added missing rdn_in input

--- rtl/Con_ID.sv
+++ rtl/Con_ID.sv
@@ -3,6 +3,7 @@ module Con_ID #(
 )(
   input clk, cache_clk, rstn, wbe, addr_mode, branch_taken,
   input [2:0] immode,
+  input [4:0] rdn_in;
   input[WordSize - 1:0] ins, pc_in, rdd,
   output logic [4:0] rdn,
   output logic [WordSize - 1:0] imm, pc, rs1d, rs2d, npc, branch_addr
@@ -12,7 +13,7 @@ logic [4:0] rs1n, rs2n;
 
 IFID if_id_latch(.clk(clk), .rstn(rstn), .immode(immode), .ins(ins), .pc_in(pc_in), .rdn(rdn), .rs1n(rs1n), .rs2n(rs2n), .imm(imm), .pc(pc));
 
-GPR registers(.clk(cache_clk), .rstn(rstn), .wbe(wbe), .rs1n(rs1n), .rs2n(rs2n), .rdn(rdn), .rdd(rdd), .rs1d(rs1d), .rs2d(rs2d));
+GPR registers(.clk(cache_clk), .rstn(rstn), .wbe(wbe), .rs1n(rs1n), .rs2n(rs2n), .rdn(rdn_in), .rdd(rdd), .rs1d(rs1d), .rs2d(rs2d));
 
 Branch_Addr_Calc branch_address(.addr_mode(addr_mode), .branch_taken(branch_taken), .imm(imm), .rs1d(rs1d), .pc_in(pc), .branch_addr(branch_addr), .npc(npc));
 
