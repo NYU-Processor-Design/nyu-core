db478050289deff0705d2d28fce9f275c6a68d6b
Xingzhi Dai <xd2305@nyu.edu>
2024-02-03 18:28:12 -0500

    fix

--- rtl/Con_Branch_Cont.sv
+++ rtl/Con_Branch_Cont.sv
@@ -3,7 +3,7 @@ module Con_Branch_Cont #(
 )(
     input clk,rstn,rstn_h,pred_taken,
     input [1:0] branch_occr, branch_cond,
-    input [WordSize-1:0] pred_pc, pred_adder,alu_out,npc_in,
+    input [WordSize-1:0] pred_pc, pred_addr,alu_out,npc_in,
     output logic [WordSize-1:0] npc,
     output logic rstn_out,branch_taken
 );
@@ -13,7 +13,7 @@ logic [WordSize-1:0] npc_corr;
 
 Pipeline_Reset #(WordSize) pipeline_reset_module(.npc_in(npc_in),.npc_corr(npc_corr),.flush(flush),.npc(npc),.rstn_out(rstn_out));
 Branch_Predictor branch_predictor(.clk(clk),.rstn_h(rstn_h),.act_taken(act_taken),.pred_taken(pred_taken),.branch_occr(branch_occr),.branch_cond(branch_cond),.branch_taken(branch_taken));
-Branch_Manager  branch_manager(.clk(clk),.rstn(rstn),.pred_taken(pred_taken),.pred_pc(pred_pc),.pred_adder(pred_adder),.act_taken(act_taken),.flush(flush),.npc(npc));
+Branch_Manager  branch_manager(.clk(clk),.rstn(rstn),.pred_taken(pred_taken),.pred_pc(pred_pc),.pred_addr(pred_addr),.act_taken(act_taken),.flush(flush),.npc(npc));
 Branch_Eval #(WordSize) branch_eval(.alu_out(alu_out),.branch_cond(branch_cond),.act_taken(act_taken));
 
 endmodule
