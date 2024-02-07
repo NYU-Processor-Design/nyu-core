948ade616daa649d362d2680bab63b4748e4facd
Vito Gamberini <vito@gamberini.email>
2024-02-06 21:21:38 -0500

    refactor: Formatting and syntax fixes
    Build:
    * Update vcpkg bootstrapping to new style
    * Inline vcpkg-configuration.json
    * Update baselines
    
    Style:
    * Reformat all system verilog files with verible-format
    * " or " -> ", "
    
    Function:
    * Fix all verilog-style always blocks -> SV-style always_ff blocks
    * Fix all blocking assignments in non-blocking contexts
    * Fix a couple missing semi-colons
    * Normalize all port lists, necessary for some synthesizers (Quartus)
    
    Refactor:
    * Significantly simplify Alu.sv, don't try to out-smart the synthesizer
    * Avoid unnecessary clock-gate latch in Branch_Manager.sv

--- rtl/Con_ID.sv
+++ rtl/Con_ID.sv
@@ -1,20 +1,61 @@
 module Con_ID #(
-  WordSize = 32
-)(
-  input clk, cache_clk, rstn, wbe, addr_mode, branch_taken,
-  input [2:0] immode,
-  input [4:0] rdn_in;
-  input[WordSize - 1:0] ins, pc_in, rdd,
-  output logic [4:0] rdn,
-  output logic [WordSize - 1:0] imm, pc, rs1d, rs2d, npc, branch_addr
+    WordSize = 32
+) (
+    input clk,
+    input cache_clk,
+    input rstn,
+    input wbe,
+    input addr_mode,
+    input branch_taken,
+    input [2:0] immode,
+    input [4:0] rdn_in,
+    input [WordSize - 1:0] ins,
+    input [WordSize - 1:0] pc_in,
+    input [WordSize - 1:0] rdd,
+    output logic [4:0] rdn,
+    output logic [WordSize - 1:0] imm,
+    output logic [WordSize - 1:0] pc,
+    output logic [WordSize - 1:0] rs1d,
+    output logic [WordSize - 1:0] rs2d,
+    output logic [WordSize - 1:0] npc,
+    output logic [WordSize - 1:0] branch_addr
 );
-  
-logic [4:0] rs1n, rs2n;
 
-IFID if_id_latch(.clk(clk), .rstn(rstn), .immode(immode), .ins(ins), .pc_in(pc_in), .rdn(rdn), .rs1n(rs1n), .rs2n(rs2n), .imm(imm), .pc(pc));
+  logic [4:0] rs1n, rs2n;
 
-GPR registers(.clk(cache_clk), .rstn(rstn), .wbe(wbe), .rs1n(rs1n), .rs2n(rs2n), .rdn(rdn_in), .rdd(rdd), .rs1d(rs1d), .rs2d(rs2d));
+  IFID if_id_latch (
+      .clk(clk),
+      .rstn(rstn),
+      .immode(immode),
+      .ins(ins),
+      .pc_in(pc_in),
+      .rdn(rdn),
+      .rs1n(rs1n),
+      .rs2n(rs2n),
+      .imm(imm),
+      .pc(pc)
+  );
 
-Branch_Addr_Calc branch_address(.addr_mode(addr_mode), .branch_taken(branch_taken), .imm(imm), .rs1d(rs1d), .pc_in(pc), .branch_addr(branch_addr), .npc(npc));
+  GPR registers (
+      .clk (cache_clk),
+      .rstn(rstn),
+      .wbe (wbe),
+      .rs1n(rs1n),
+      .rs2n(rs2n),
+      .rdn (rdn_in),
+      .rdd (rdd),
+      .rs1d(rs1d),
+      .rs2d(rs2d)
+  );
+
+  Branch_Addr_Calc branch_address (
+      .addr_mode(addr_mode),
+      .branch_taken(branch_taken),
+      .imm(imm),
+      .rs1d(rs1d),
+      .pc_in(pc),
+      .branch_addr(branch_addr),
+      .npc(npc)
+  );
 
 endmodule
