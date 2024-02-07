44066ae94f17283d66e859010c304d406e9d3151
Vito Gamberini <vito@gamberini.email>
2024-02-06 21:21:38 -0500

    refactor: Formatting and syntax fixes
    Build:
    * Update vcpkg bootstrapping to new style
    * Inline vcpkg-configuration.json
    * Update baselines
    
    Style:
    * Reformat all system verilog files with verible-format
    * " or " -> ", "
    
    Function:
    * Fix all verilog-style always blocks -> SV-style always_ff blocks
    * Fix all blocking assignments in non-blocking contexts
    * Fix a couple missing semi-colons
    * Normalize all port lists, necessary for some synthesizers (Quartus)
    
    Refactor:
    * Significantly simplify Alu.sv, don't try to out-smart the synthesizer
    * Avoid unnecessary clock-gate latch in Branch_Manager.sv

--- rtl/EXMEM.sv
+++ rtl/EXMEM.sv
@@ -1,20 +1,22 @@
 module EXMEM #(
-  WordSize = 32
-)(
-  input clk, rstn,
-  input [4:0] rdn_in,
-  input [WordSize - 1:0] alu_out_in, rs2d,
-  output logic [4:0] rdn,
-  output logic [WordSize - 1:0] alu_out, mem_data
+    WordSize = 32
+) (
+    input clk,
+    input rstn,
+    input [4:0] rdn_in,
+    input [WordSize - 1:0] alu_out_in,
+    input [WordSize - 1:0] rs2d,
+    output logic [4:0] rdn,
+    output logic [WordSize - 1:0] alu_out,
+    output logic [WordSize - 1:0] mem_data
 );
 
-  always @ (posedge clk or negedge rstn) begin
+  always_ff @(posedge clk, negedge rstn) begin
     if (!rstn) begin
       rdn <= 0;
       alu_out <= 0;
       mem_data <= 0;
-    end
-    else begin
+    end else begin
       rdn <= rdn_in;
       alu_out <= alu_out_in;
       mem_data <= rs2d;
--- rtl/GPR.sv
+++ rtl/GPR.sv
@@ -1,30 +1,30 @@
 module GPR #(
-  WordSize = 32
-)(
-  input clk, rstn, wbe,
-  input [4:0] rs1n, rs2n, rdn,
-  input [WordSize - 1:0] rdd,
-  output logic [WordSize - 1:0] rs1d, rs2d
+    WordSize = 32
+) (
+    input clk,
+    input rstn,
+    input wbe,
+    input [4:0] rs1n,
+    input [4:0] rs2n,
+    input [4:0] rdn,
+    input [WordSize - 1:0] rdd,
+    output logic [WordSize - 1:0] rs1d,
+    output logic [WordSize - 1:0] rs2d
 );
-  
-  logic [31:0] registers [31:1]; 
-  
-  integer i;
 
-  //if this doesn't work, try setting regs to 0 outside of always 
+  logic [31:0] registers[31:1];
 
-  always @ (posedge clk or negedge rstn) begin
-    if (rstn == 0)
-      begin
-        for (i = 1; i < 32; i = i+1) begin
-          registers[i] <= 32'd0;
-        end
+  //if this doesn't work, try setting regs to 0 outside of always
+
+  always_ff @(posedge clk, negedge rstn) begin
+    if (rstn == 0) begin
+      for (int i = 1; i < 32; i = i + 1) begin
+        registers[i] <= 32'd0;
       end
-    else begin
+    end else begin
       //rs1d <= registers[rs1n];
       //rs2d <= registers[rs2n];
-      if (wbe && rdn != 0)
-        registers[rdn] <= rdd;
+      if (wbe && rdn != 0) registers[rdn] <= rdd;
     end
   end
 
