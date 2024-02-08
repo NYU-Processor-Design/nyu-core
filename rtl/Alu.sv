8d6a2cdfe0bd16269373173470731160189f6d45
ShinyMiraidon <114675487+ShinyMiraidon@users.noreply.github.com>
2024-02-08 10:41:49 -0500

    Revert "refactor: Formatting and syntax fixes"
    This reverts commit 97856a1be6aa2b3cae415ed4073d625b9b12b4a2.

--- rtl/Alu.sv
+++ rtl/Alu.sv
@@ -13,26 +13,37 @@ parameter USLT = 6'h03;
 
 
 module Alu #(
-    WordSize = 32
-) (
-    input [WordSize - 1:0] a,
-    input [WordSize - 1:0] b,
+  WordSize = 32
+)(
+    input [WordSize - 1:0] a, b,
     input [5:0] alu_mode,
     output logic [WordSize - 1:0] alu_out
 );
-  always_comb begin
-    case (alu_mode)
-      ADD: alu_out = a + b;
-      SUB: alu_out = a - b;
-      XOR: alu_out = a ^ b;
-      OR: alu_out = a | b;
-      AND: alu_out = a & b;
-      LLS: alu_out = a << b[4:0];
-      LRS: alu_out = a >> b[4:0];
-      ARS: alu_out = $signed(a) >>> b[4:0];
-      SSLT: alu_out = WordSize'($signed(a) < $signed(b));
-      USLT: alu_out = WordSize'(a < b);
-      default: alu_out = 0;
+
+    logic[WordSize - 1:0] adder_result;
+    logic do_sub, carry;
+
+assign do_sub = alu_mode[5] | (alu_mode == SSLT) | (alu_mode == USLT);
+
+assign {carry, adder_result} = {1'b0, a} + {1'b0, ((b^({WordSize{do_sub}})) + {{WordSize - 1{1'b0}}, do_sub})};
+
+always_comb begin 
+    case(alu_mode)
+    ADD: alu_out = adder_result;
+    SUB: alu_out = adder_result;
+    XOR: alu_out = a ^ b;
+    OR: alu_out = a | b;
+    AND: alu_out = a & b;
+    LLS: alu_out = a << b[4:0];
+    LRS: alu_out = a >> b[4:0];
+    ARS: alu_out = a >>> b[4:0];
+    SSLT: alu_out = (a[WordSize - 1] & !(b[WordSize - 1])) ? 1 : (!(a[WordSize - 1]) & b[WordSize - 1]) ? 0 : {{WordSize - 1{1'b0}}, carry^(!b[WordSize - 1])};
+    USLT: alu_out = {{WordSize - 1{1'b0}}, !carry};
+    default: alu_out = 0;
     endcase
-  end
+
+
+end
+
+
 endmodule
--- rtl/IDEX.sv
+++ rtl/IDEX.sv
@@ -1,27 +1,16 @@
 module IDEX #(
-    WordSize = 32
-) (
-    input clk,
-    input rstn,
-    input branch_taken_in,
-    input [1:0] a_sel,
-    input [1:0] b_sel,
-    input [WordSize - 1:0] pc_in,
-    input [WordSize - 1:0] imm,
-    input [WordSize - 1:0] rs1d,
-    input [WordSize - 1:0] rs2d_in,
-    input [WordSize - 1:0] branch_addr_in,
-    input [4:0] rdn_in,
-    output logic branch_taken,
-    output logic [4:0] rdn,
-    output logic [WordSize - 1:0] pc,
-    output logic [WordSize - 1:0] branch_addr,
-    output logic [WordSize - 1:0] a,
-    output logic [WordSize - 1:0] b,
-    output logic [WordSize - 1:0] rs2d
+  WordSize = 32
+)(
+  input clk, rstn, branch_taken_in,
+  input[1:0] a_sel, b_sel,
+  input[WordSize - 1:0] pc_in, imm, rs1d, rs2d_in, branch_addr_in,
+  input[4:0] rdn_in,
+  output logic branch_taken,
+  output logic [4:0] rdn,
+  output logic [WordSize - 1:0] pc, branch_addr, a, b, rs2d
 );
-
-  always @(posedge clk or negedge rstn) begin
+  
+  always @ (posedge clk or negedge rstn) begin
     if (!rstn) begin
       rdn <= 0;
       a <= 0;
@@ -30,19 +19,20 @@ module IDEX #(
       pc <= 0;
       rs2d <= 0;
       branch_taken <= 0;
-    end else begin
+    end
+    else begin
       rdn <= rdn_in;
       branch_taken <= branch_taken_in;
       rs2d <= rs2d_in;
       branch_addr <= branch_addr_in;
       pc <= pc_in;
-      case (a_sel)
+      case(a_sel)
         0: a <= rs1d;
         1: a <= pc_in;
         3: a <= 0;
-        default a <= 0;
+      default a <= 0;
       endcase
-      case (b_sel)
+      case(b_sel)
         0: b <= rs2d_in;
         1: b <= imm;
         2: b <= 4;
