8a84a2515a6b57beffd5484986db3509f01b9afa
ShinyMiraidon <114675487+ShinyMiraidon@users.noreply.github.com>
2024-02-08 10:41:49 -0500

    Revert "refactor: Formatting and syntax fixes"
    This reverts commit 97856a1be6aa2b3cae415ed4073d625b9b12b4a2.

--- rtl/PC.sv
+++ rtl/PC.sv
@@ -1,18 +1,20 @@
 
 module PC #(
-    WordSize = 32
-) (
-    input clk,
-    input rstn,
-    input pc_en,
-    input [WordSize - 1:0] npc,
-    output logic [WordSize - 1:0] pc
+  WordSize = 32
+)(
+  input clk,
+  input rstn,
+  input pc_en,
+  input [WordSize - 1:0] npc,
+  output logic [WordSize - 1:0] pc
 );
-
-  always_ff @(posedge clk, negedge rstn) begin
-    if (rstn == 0) pc <= 0;
+  
+  always @ (posedge clk or negedge rstn) begin
+    if (rstn == 0)
+      pc <= 0;
     else begin
-      if (pc_en) pc <= npc;
+      if (pc_en)
+        pc <= npc;
     end
   end
 endmodule
--- rtl/Pipeline_Reset.sv
+++ rtl/Pipeline_Reset.sv
@@ -1,23 +1,22 @@
 module Pipeline_Reset #(
-    WordSize = 32
-) (
-    input [WordSize - 1:0] npc_in,
-    input [WordSize - 1:0] npc_corr,
-    input flush,
-    output logic [WordSize - 1:0] npc,
-    output logic rstn_out
+  WordSize = 32
+)(
+  input [WordSize - 1:0] npc_in, npc_corr,
+  input flush,
+  output logic [WordSize - 1:0] npc,
+  output logic rstn_out
 );
 
-  always_comb begin
-    case (flush)
-      0: begin
-        npc = npc_in;
-        rstn_out = 1'bZ;
-      end
-      1: begin
-        npc = npc_corr;
-        rstn_out = 0;
-      end
-    endcase
-  end
+always_comb begin
+case(flush)
+0: begin
+    npc = npc_in;
+    rstn_out = 1'bZ;
+end
+1: begin
+    npc = npc_corr;
+    rstn_out = 0;
+end
+endcase
+end
 endmodule
