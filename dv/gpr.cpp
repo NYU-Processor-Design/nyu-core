d7cb03b404f5c6c472c209dfc16ddaae5c0b8e0b
ShinyMiraidon <bind-naps0r@icloud.com>
2024-02-12 16:32:27 -0500

    more updating of gpr test

--- dv/gpr.cpp
+++ dv/gpr.cpp
@@ -48,20 +48,43 @@ static void eval_rstn() {
   nyu::eval(gpr);
 
   for(std::uint8_t rs1n {0}; rs1n < 32; ++rs1n)
+    INFO("Testing Register " << rs1n);
     REQUIRE(read(gpr, rs1n, 0)[0] == 0);
 }
 
-static void eval() {
+static void eval(std::uint32_t[32] reg_vals, bool test_wbe) {
   auto& gpr {nyu::getDUT<VGPR>()};
+  (std::uint32_t, std::uint32_t) read_vals;
+  
+  for(std::uint8_t rdn {0}; rdn < 32; ++rdn)
+    write(gpr, rdn, reg_vals[rdn], 1);
 
+  reg_vals[0] = 0;
+
+  if(test_wbe) {
+    for(std::uint8_t rdn {0}; rdn < 32; ++rdn)
+      write(gpr, rdn, 0xFF, 0);
+  }
+
+  for(std::uint8_t rs1n {0}; rs1n < 32; ++rs1n)
+    for(std::uint8_t rs2n {0}; rs2n < 32; ++rs2n) {
+      read_vals = read(gpr, rs1n, rs2n);
+      INFO("Testing rs1n = " << rs1n << ", rs2n = " << rs2n);
+      REQUIRE(read_vals[0] == reg_vals[rs1n]);
+      REQUIRE(read_vals[1] == reg_vals[rs2n]);
+    }
+}
+
+TEST_CASE("GPR, Reset") {
+  eval_rstn();
+}
+
+TEST_CASE("GPR, Write and Read") {
   
 
 }
 
-TEST_CASE("GPR, Reset") {
+TEST_CASE("GPR, Disable Writing") {
 
-}
-
-TEST_CASE("GPR, ") {
 
 }
