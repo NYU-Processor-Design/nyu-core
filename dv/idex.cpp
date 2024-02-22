f9fb71923c54bc843b6a08010b9bc1058be5789e
ShinyMiraidon <bind-naps0r@icloud.com>
2024-02-22 14:15:42 -0500

    test(idex.cpp): Even faster test

--- dv/idex.cpp
+++ dv/idex.cpp
@@ -79,11 +79,9 @@ static void a_test(std::uint8_t a_sel) {
         for (int branch_taken_in {0}; branch_taken_in < 2; ++branch_taken_in)
             for (std::uint8_t rdn_in {1}; rdn_in < 32; rdn_in <<= 1)
                 for (std::uint32_t pc_in {1}; pc_in; pc_in <<= 1)
-                    for (std::uint32_t imm {1}; imm; imm <<= 1)
                         for (std::uint32_t rs1d {1}; rs1d; rs1d <<= 1)
-                            for (std::uint32_t rs2d_in {1}; rs2d_in; rs2d_in <<= 1)
                                 for (std::uint32_t branch_addr_in {1}; branch_addr_in; branch_addr_in <<= 1)
-                                    eval(idex, a_sel, b_sel, branch_taken_in, rdn_in, pc_in, imm, rs1d, rs2d_in, branch_addr_in);
+                                    eval(idex, a_sel, b_sel, branch_taken_in, rdn_in, pc_in, 0, rs1d, 0, branch_addr_in);
 }
 
 static void b_test(std::uint8_t b_sel) {
@@ -94,12 +92,10 @@ static void b_test(std::uint8_t b_sel) {
     for (std::uint8_t a_sel {1}; a_sel < 4; a_sel <<= 1)
         for (int branch_taken_in {0}; branch_taken_in < 2; ++branch_taken_in)
             for (std::uint8_t rdn_in {1}; rdn_in < 32; rdn_in <<= 1)
-                for (std::uint32_t pc_in {1}; pc_in; pc_in <<= 1)
                     for (std::uint32_t imm {1}; imm; imm <<= 1)
-                        for (std::uint32_t rs1d {1}; rs1d; rs1d <<= 1)
                             for (std::uint32_t rs2d_in {1}; rs2d_in; rs2d_in <<= 1)
                                 for (std::uint32_t branch_addr_in {1}; branch_addr_in; branch_addr_in <<= 1)
-                                    eval(idex, a_sel, b_sel, branch_taken_in, rdn_in, pc_in, imm, rs1d, rs2d_in, branch_addr_in);
+                                    eval(idex, a_sel, b_sel, branch_taken_in, rdn_in, 0, imm, 0, rs2d_in, branch_addr_in);
 }
 
 TEST_CASE("IDEX, a_sel = 0") {
